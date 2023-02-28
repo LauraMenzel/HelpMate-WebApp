c780da9776f3f2cf3de83f347a173153490dc18a
LauraMenzel <menzel-laura@gmx.de>
2023-02-28 10:47:36 +0100

    add changes in editprofil

--- client/src/components/Profile/EditProfile.js
+++ client/src/components/Profile/EditProfile.js
@@ -1,9 +1,12 @@
-import { FiUser } from "react-icons/fi";
+import { BsPersonLinesFill } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
+import { TbLanguage } from "react-icons/tb";
 import { GoLocation } from "react-icons/go";
 import { BsFillCalendarMonthFill } from "react-icons/bs";
 import axios from "axios";
+import { MdArrowBackIosNew } from "react-icons/md";
 import { Link } from "react-router-dom";
+import {FaHandsHelping} from "react-icons/fa"
 
 import noImg from "../../images/no-img.jpg";
 import { useContext, useState, useEffect } from "react";
@@ -58,91 +61,62 @@ function EditProfile() {
       url: URL.createObjectURL(e.currentTarget.files[0]),
       file: e.currentTarget.files[0],
     });
-  };
+  }
 
   return (
-    <div className="flex w-full h-full justify-center items-center gap-[20px] bg-neutral-100 flex-col">
-      <Link className="hover:text-red-500" to="/home">
-        go to home
-      </Link>
-      
-      <img
-        className="w-[150px] h-[150px] rounded-md object-cover"
-        src={fileData.url || noImg}
-        alt=""
-      />
-      <div className="flex items-center gap-[10px]">
-        <FiUser className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.username}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          onChange={(e) => setData({ ...data, username: e.target.value })}
-        />
-      </div>
-      <div className="flex items-center gap-[10px]">
-        <FiUser className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.firstname}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          onChange={(e) => setData({ ...data, firstname: e.target.value })}
-        />
-      </div>
-      <div className="flex items-center gap-[10px]">
-        <FiUser className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.lastname}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          onChange={(e) => setData({ ...data, lastname: e.target.value })}
-        />
-      </div>
-      <div className="flex items-center gap-[10px]">
-        <HiOutlineMail className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.email}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          placeholder=""
-          onChange={(e) => setData({ ...data, email: e.target.value })}
-        />
-      </div>
-      <div className="flex items-center gap-[10px]">
-        <HiOutlineMail className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.phonenumber}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          placeholder=""
-          onChange={(e) => setData({ ...data, phonenumber: e.target.value })}
-        />
+    <div className="bg-[#E3DDDD] h-full">
+      <div className="flex justify-start ml-[30px] max-w-[550px] min-w-[340px] md:w-3/4  lg:w-1/2  pt-8">
+        <Link to="/home">
+          <MdArrowBackIosNew className="hover:text-red-500 bg-white rounded-3xl  text-[30px]" />
+        </Link>
       </div>
 
-      <div className="flex items-center gap-[10px]">
-        <GoLocation className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.city}
-          onChange={(e) => setData({ ...data, city: e.target.value })}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
+      <div className="flex w-full h-full justify-center items-center gap-[20px] flex-col">
+        <img
+          className="w-[150px] h-[150px] rounded-3xl mb-5 object-cover"
+          src={fileData.url || noImg}
+          alt="profilpicture"
         />
+        <div className="flex items-center gap-[10px]">
+          <BsPersonLinesFill className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-3xl p-[3px]" />
+
+          <input
+            value={data.username}
+            className="border-2 rounded-3xl border-slate-500 p-[5px] w-[200px] h-[40px]"
+            onChange={(e) => setData({ ...data, username: e.target.value })}
+          />
+        </div>
+        <div className="flex items-center gap-[10px]">
+          <TbLanguage className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-3xl p-[3px]" />
+
+          <input
+            value={data.firstname}
+            className="border-2 border-slate-500 rounded-3xl p-[5px] w-[200px] h-[40px]"
+            onChange={(e) => setData({ ...data, firstname: e.target.value })}
+          />
+        </div>
+        <div className="flex items-center gap-[10px]">
+          <FaHandsHelping className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-3xl p-[3px]" />
+
+          <input
+            value={data.lastname}
+            className="border-2 border-slate-500 rounded-3xl p-[5px] w-[200px] h-[40px]"
+            onChange={(e) => setData({ ...data, lastname: e.target.value })}
+          />
+        </div>
+        <div className="w-3/4 max-w-[150px] mb-12">
+          <button
+            onClick={handleSave}
+            className="py-2 bg-[#3B8A80] w-full rounded-3xl 
+              text-white font-bold hover:bg-[#70c2b7] active:bg-[#3d8f84]"
+          >
+            Update profile
+          </button>
+        </div>
       </div>
-
-      <div className="flex items-center gap-[10px]">
-        <BsFillCalendarMonthFill className="text-slate-400 w-[40px] h-[40px] border-2 border-slate-400 rounded-md p-[3px]" />
-
-        <input
-          value={data.age}
-          onChange={(e) => setData({ ...data, age: e.target.value })}
-          className="border-2 border-slate-500 p-[5px] w-[200px] h-[40px]"
-          placeholder=""
-        />
-      </div>
-
-      <button onClick={handleSave}>Update profile</button>
     </div>
   );
 }
+
 
 export default EditProfile;
